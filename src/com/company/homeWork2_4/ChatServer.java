3cc097e67ee86b6225f20ee49b6a512c17644d9b
Roman <katyrinroman@mail.ru>
2020-10-18 22:04:10 +0700

    add directory homeWork2_3 and files

---
+++ src/com/company/homeWork2_4/ChatServer.java
@@ -0,0 +1,10 @@
+package com.company.homeWork2_4;
+
+public class ChatServer {
+    public void start(int port){
+        System.out.println("Server started at port: " + port);
+    }
+    public void stop(){
+        System.out.println("Server stopped");
+    }
+}
